64983f9ef0804a64a81f374077ecc8a774954de6
김원혁 <kwh102455@gmail.com>
2025-11-19 23:10:01 +0900

    feat(service): 답변 서비스 구현

---
+++ springboot_study/src/main/java/study/crud/springboot_study/service/AnswerService.java
@@ -0,0 +1,42 @@
+package study.crud.springboot_study.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import study.crud.springboot_study.domain.Answer;
+import study.crud.springboot_study.domain.Question;
+import study.crud.springboot_study.exception.answer_exception.AnswerContentBlankException;
+import study.crud.springboot_study.exception.answer_exception.AnswerExceptionMessageEnum;
+import study.crud.springboot_study.repository.AnswerRepository;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    /**
+     * 질문 상세 정보 페이지에 존재하는 답변다는 창 textarea 태그의 내용을 이용
+     * QuestionResponseDto를 인수로 사용하려 했지만, Answer domain은 dto가 아닌 question domain에 의존하여 dto를 못쓴다.
+     * @param question 답변 달릴 질문글에 대한 객체
+     * @param content input 태그의 내용
+     */
+    public void createAnswer(Question question, String content) {
+        checkContent(content);
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        answerRepository.save(answer);
+    }
+
+    /**
+     * 답변글에 대한 검증 답변내용이 비어있거나 null이면 예외 발생
+     * @param content 답변 내용
+     */
+    public void checkContent(String content) {
+        if (content==null || content.isEmpty()) {
+            throw new AnswerContentBlankException(AnswerExceptionMessageEnum.BLANK_ANSWER_CONTENT_EXCEPTION);
+        }
+    }
+}
