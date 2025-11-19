e974603590c9a0863fcdc56fdab72be23dcffd72
김원혁 <kwh102455@gmail.com>
2025-11-19 23:07:26 +0900

    refactor(dto): 질문 요청, 응답 dto 수정

--- springboot_study/src/main/java/study/crud/springboot_study/dto/QuestionResponse.java
+++ springboot_study/src/main/java/study/crud/springboot_study/dto/QuestionResponse.java
@@ -3,19 +3,30 @@ package study.crud.springboot_study.dto;
 import study.crud.springboot_study.domain.Question;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+/**
+ * Question응답 DTO이다. question 엔티티에 반드시 필요한 맴버들이 존재
+ * @param id question id
+ * @param subject question title
+ * @param content question content
+ * @param createDate question create date
+ * @param answerList 자식 엔티티인 모든 answer 엔티티
+ */
 public record QuestionResponse (
         Long id,
         String subject,
         String content,
-        LocalDateTime createDate
+        LocalDateTime createDate,
+        List<AnswerResponse> answerList
 ) {
     public static QuestionResponse from(Question q) {
         return new QuestionResponse(
                 q.getId(),
                 q.getSubject(),
                 q.getContent(),
-                q.getCreateDate()
+                q.getCreateDate(),
+                q.getAnswerList().stream().map(AnswerResponse::from).toList()
         );
     }
 }
