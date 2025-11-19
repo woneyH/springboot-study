7e3edbe6fb99ae125bed09502c237c461307e3ee
김원혁 <kwh102455@gmail.com>
2025-11-19 23:09:12 +0900

    feat(exception): 답변 예외 기능들 추가.

---
+++ springboot_study/src/main/java/study/crud/springboot_study/exception/answer_exception/GlobalAnswerExceptionHandler.java
@@ -0,0 +1,25 @@
+package study.crud.springboot_study.exception.answer_exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+@Slf4j
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class GlobalAnswerExceptionHandler {
+    @ExceptionHandler(AnswerException.class)
+    public String handleAnswerException(AnswerException e, Model model) {
+        log.error("AnswerException 발생",e.getMessage(),e);
+
+        model.addAttribute("errorMessage",e.getMessage());
+        model.addAttribute("errorCode",e.getHttpStatus());
+        model.addAttribute("timestamp", LocalDateTime.now());
+        return "error";
+    }
+}
